a8a7a043cbfa089a7873d98992dd96b93d60af60
umbur <work1415@gmail.com>
2019-01-30 14:54:34 -0700

    Initial commit, plus installed dependencies

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,24 @@
+import axios from 'axios';
+
+export const FETCH_SWAPI_START = 'FETCH_SWAPI_START';
+export const FETCH_SWAPI_SUCCESS = 'FETCH_SWAPI_SUCCESS';
+export const FETCH_SWAPI_FAILURE = 'FETCH_SWAPI_FAILURE';
+
+export const fetchSwapi = () => dispatch => {
+    dispatch({ type: FETCH_SWAPI_START });
+    axios
+        .get('https://swapi.co/api/people')
+        .then(response => {
+            dispatch({ type: FETCH_SWAPI_SUCCESS, payload: response.data.results });
+        })
+        .catch(err => {
+            dispatch({ type: FETCH_SWAPI_FAILURE, payload: err })
+        });
+};
+
+
+
+
 // we'll need axios
 
 // we'll need to create 3 different action types here.
